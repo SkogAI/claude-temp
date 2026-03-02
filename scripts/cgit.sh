47192cfc81b8971d9688407959fff81d7e7a869a
skogix <emil@skogsund.se>
2026-03-03 00:52:20 +0100

    fetch docs and remove cgit

--- scripts/cgit.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Wrapper for bare git repo claude global
-# Git dir: /mnt/sda1/claude-global.git
-# Work tree: $HOME
-
-exec git --git-dir="/mnt/sda1/claude-global.git" --work-tree="/home/skogix" "$@"

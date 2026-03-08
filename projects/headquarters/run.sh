0d2abbfaf48e3ef8d7fd302024359823eca81b27
Emil Skogsund <emil@skogsund.se>
2026-03-08 07:47:30 +0100

    commit from run.sh

--- projects/headquarters/run.sh
+++ projects/headquarters/run.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
 echo "password1" >bootstrap/pat.password.example
+git add -A
+git commit -m "commit from run.sh"
+git push
 podman pod rm -f pod_headquarters
 podman network rm headquarters_default
 podman rm -f soft-serve

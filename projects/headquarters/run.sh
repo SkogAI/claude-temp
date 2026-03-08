64e3495e70ad3664e5656ee12920260326f4cca4
Emil Skogsund <emil@skogsund.se>
2026-03-08 07:27:04 +0100

    .

--- projects/headquarters/run.sh
+++ projects/headquarters/run.sh
@@ -4,4 +4,4 @@ echo "password1" >bootstrap/pat.password.example
 docker compose build
 docker compose down
 docker compose up -d
-docker exec soft-serve bash -c "cd /bootstrap && git pull && ./bootstrap.sh"
+docker exec soft-serve bash -c "git clone https://github.com/SkogAI/bootstrap.git ~/bootstrap && cd ~/bootstrap && ./bootstrap.sh"
